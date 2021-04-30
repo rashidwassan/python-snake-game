4d6da89e70f5e971b08e15f07ac42652cf34961f
rashidwassan <rashidwassaan@gmail.com>
2021-04-30 22:52:34 +0500

    Fixed food appearance out of viewport

--- food.py
+++ food.py
@@ -13,6 +13,6 @@ class Food(Turtle):
         self.refresh()
 
     def refresh(self):
-        random_x = random.randint(-340, 340)
-        random_y = random.randint(-480, 480)
+        random_x = random.randint(-480, 480)
+        random_y = random.randint(-340, 340)
         self.goto(random_x, random_y)
