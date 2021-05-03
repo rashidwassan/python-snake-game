6356378798211c70bffa350532df1f7b0a2d834d
rashidwassan <rashidwassaan@gmail.com>
2021-05-03 20:40:11 +0500

    Preserve high score in txt file.

--- scoreboard.py
+++ scoreboard.py
@@ -6,7 +6,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('data.txt', 'r') as data:
+            self.high_score = int(data.read())
         self.penup()
         self.color('white')
         self.goto(0, 340) 
@@ -24,5 +25,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+        file = open('data.txt', 'w')
+        file.write(str(self.high_score))
+        file.close()
         self.score = 0
         self.update_scoreboard()
